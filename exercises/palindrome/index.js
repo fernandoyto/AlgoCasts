49ca31327defa15635da66a10d17fa6f44d1c2f5
fernandoyto <fernando.toshioyto@gmail.com>
2020-03-27 19:22:38 -0300

    palindrome done

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,9 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    let reversed = str.split('').reduce((newStr, currentChar) => currentChar + newStr, '');
+    return str === reversed;
+}
 
 module.exports = palindrome;
