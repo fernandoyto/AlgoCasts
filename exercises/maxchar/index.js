864a4925345310d6e9b734538f74d2e9b9dc53a3
fernandoyto <fernando.toshioyto@gmail.com>
2020-03-27 23:59:24 -0300

    maxChar done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const obj = {}
+    for (let char of str) {
+        if (obj[char] === undefined) {
+            obj[char] = 1;
+        } else {
+            obj[char]++;
+        }
+    }
+    return Object.keys(obj).reduce((max, current) => obj[current] > max ? obj[current] : max);
+}
+
+maxChar('aabbbcdeee')
 
 module.exports = maxChar;
